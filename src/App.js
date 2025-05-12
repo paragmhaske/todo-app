a1206d28933badd643bbd0c277841b7dc42efb6a
Parag Mhaske <parag.mhaske@otis.com>
2025-05-12 17:25:27 +0530

    Initial commit - To-Do app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import logo from './logo.svg';
+import React, { useState } from "react";
+
 import './App.css';
 
 function App() {
+  const [task, setTask] = useState(""); // for input
+  const [tasks, setTasks] = useState([]); // list of tasks
+
+  const handleAdd = () => {
+    if (task.trim() !== "") {
+      setTasks([...tasks, task]);
+      setTask(""); // clear input
+    }
+  };
   return (
-    <div className="App">
+    <div>
+    <h1>My To-Do List</h1>
+    <input type="text" value={task}
+        onChange={(e) => setTask(e.target.value)} placeholder="Add a new task..." />
+    <button  onClick={handleAdd} style={{ marginLeft: "10px"} }>Add</button>  
+    <ul style={{ marginTop: "20px" }}>
+        {tasks.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+  </div>
+    /*<div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,7 +40,8 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
+
+    </div>*/
   );
 }
 
